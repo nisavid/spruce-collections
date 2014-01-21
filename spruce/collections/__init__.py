374978dc6e75e008b11ed150df2d0c4ab034406a
Ivan D Vasin <ivan@pixability.com>
2014-01-21 08:40:35 -0500

    improve project description

--- spruce/collections/__init__.py
+++ spruce/collections/__init__.py
@@ -1,6 +1,17 @@
 """Container data types
 
-This is an extension of :mod:`collections`.
+This is an extension of :mod:`collections` and the built-in mapping_,
+sequence_, and set_ data types.
+
+
+.. _mapping: \
+    http://docs.python.org/2/library/stdtypes.html#mapping-types-dict
+
+.. _sequence: \
+    http://docs.python.org/2/library/stdtypes.html#sequence-types-str-unicode-list-tuple-bytearray-buffer-xrange
+
+.. _set: \
+    http://docs.python.org/2/library/stdtypes.html#set-types-set-frozenset
 
 """
 
